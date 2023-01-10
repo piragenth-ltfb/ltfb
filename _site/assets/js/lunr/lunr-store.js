c1e295dfcfbffcfb07fb482792f6f29c22116a42
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2023-01-10 08:48:46 +0530

    update post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Text and Typography",
+        "excerpt":"This post is to show Markdown syntax rendering on Chirpy, you can also use it as an example of writing. Now, let’s start looking at text and typography. Titles H1 - heading H2 - heading H3 - heading H4 - heading Paragraph I wandered lonely as a cloud That floats...","categories": ["Blogging","Demo"],
+        "tags": ["typography"],
+        "url": "/blogging/demo/text-and-typography/",
+        "teaser": null
+      }]
